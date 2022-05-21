72fd290d502522f7409c11208c085ba133188a23
roger <romachad@student.42sp.org.br>
2022-05-21 05:43:43 +0200

    Completado ex09 10 11 12 e 13

---
+++ ex12/ft_iterative_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 05:15:49 by romachad          #+#    #+#             */
+/*   Updated: 2022/05/21 05:42:39 by romachad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb == 0 || nb == 1)
+		return (1);
+	else if (nb < 0)
+		return (0);
+	result = nb;
+	while (nb > 1)
+	{
+		nb--;
+		result = result * nb;
+	}
+	return (result);
+}
---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 05:27:46 by romachad          #+#    #+#             */
+/*   Updated: 2022/05/21 05:38:12 by romachad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	recursive(int nb)
+{
+	if (nb > 1)
+		nb = nb * recursive(nb - 1);
+	return (nb);
+}
+
+int	ft_recursive_factorial(int nb)
+{
+	int	result;
+
+	if (nb == 0 || nb == 1)
+		return (1);
+	else if (nb < 0)
+		return (0);
+	result = recursive(nb);
+	return (result);
+}
