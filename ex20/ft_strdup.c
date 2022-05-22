c1e6cbb9b662c856ca06742938bb4a97d3463697
roger <romachad@student.42sp.org.br>
2022-05-22 08:35:11 +0200

    Ajusted norm ex20

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 06:00:38 by romachad          #+#    #+#             */
-/*   Updated: 2022/05/22 06:19:18 by romachad         ###   ########.fr       */
+/*   Updated: 2022/05/22 08:34:06 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -21,16 +21,16 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-char *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
-	int	size;
-	int	i;
 	char	*new_str;
+	int		size;
+	int		i;
 
 	size = ft_strlen(src);
 	new_str = malloc(size * sizeof(char));
 	i = 0;
-	while(src[i])
+	while (src[i])
 	{
 		new_str[i] = src[i];
 		i++;
