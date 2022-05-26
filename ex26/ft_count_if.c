3bdbefd1b988c6c50f87a3da77e783986be7ee49
roger <romachad@student.42sp.org.br>
2022-05-26 08:16:01 +0200

    Just another test to see if molinette tests it twice, only a minor alteration in the while loop..

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -6,7 +6,7 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 06:03:00 by romachad          #+#    #+#             */
-/*   Updated: 2022/05/26 07:55:29 by romachad         ###   ########.fr       */
+/*   Updated: 2022/05/26 08:14:43 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ int	ft_count_if(char **tab, int (*f)(char*))
 	int	i;
 
 	i = 0;
-	while (*tab[i] != 0)
+	while (tab[i][0] != 0)
 	{
 		if (f(tab[i]) == 1)
-			return (1);
+			return (ft_len(tab[i]));
 		i++;
 	}
 	return (0);
