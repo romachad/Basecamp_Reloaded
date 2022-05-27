084ee8573a77ccf1d192adf577e1246f55372b08
roger <romachad@student.42sp.org.br>
2022-05-27 03:37:10 +0200

    Alright, changed ex26...seems the pdf wants to play some tricks...\nNO MORE!

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -6,30 +6,22 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 06:03:00 by romachad          #+#    #+#             */
-/*   Updated: 2022/05/26 08:14:43 by romachad         ###   ########.fr       */
+/*   Updated: 2022/05/27 03:35:34 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_len(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
 
 int	ft_count_if(char **tab, int (*f)(char*))
 {
 	int	i;
+	int	result;
 
 	i = 0;
-	while (tab[i][0] != 0)
+	result = 0;
+	while (tab[i] != 0)
 	{
 		if (f(tab[i]) == 1)
-			return (ft_len(tab[i]));
+			result++;
 		i++;
 	}
-	return (0);
+	return (result);
 }
