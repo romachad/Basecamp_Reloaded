881cc10ef0cf9f76db6ca0e3499906dc64b7b242
roger <romachad@student.42sp.org.br>
2022-05-27 06:56:46 +0200

    Finally corrected ex09. Why did it took me so long to make a simple correction you ask? Just to annoy Molli! A far more important question you should be asking yourself is: What are you doing with your life? I would assume you would have a number of better and more interesting things to do than read this last commit...

--- ex08/ft_is_negative.c
+++ ex08/ft_is_negative.c
@@ -6,7 +6,7 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 04:45:10 by romachad          #+#    #+#             */
-/*   Updated: 2022/05/21 04:46:36 by romachad         ###   ########.fr       */
+/*   Updated: 2022/05/27 06:53:15 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ void	ft_putchar(char c);
 void	ft_is_negative(int n)
 {
 	if (n < 0)
-		ft_putchar('-');
+		ft_putchar('N');
 	else
-		ft_putchar('+');
+		ft_putchar('P');
 }
